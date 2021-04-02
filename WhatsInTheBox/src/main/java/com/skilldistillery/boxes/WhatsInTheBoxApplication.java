32a72c885a10c849708eb99801ef7c4a943c0f06
Your Name <you@example.com>
2021-04-02 15:05:54 -0600

    Stub Spring Boot project, connect JPA dependency

---
+++ WhatsInTheBox/src/main/java/com/skilldistillery/boxes/WhatsInTheBoxApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.boxes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsInTheBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsInTheBoxApplication.class, args);
+	}
+
+}
